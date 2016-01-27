a1a249f182ab2826739f9835cecd07e9865a4ef5
alevento <alessandro.manneschi@gmail.com>
2016-01-27 12:40:26 +0100

    rename

--- src/TestBase.php
+++ src/TestBase.php
@@ -33,6 +33,7 @@ class TestBase extends \TestCase
      * @param string $methodName Method name to call
      * @param array  $parameters Array of parameters to pass into method.
      *
+     *
      * @return mixed Method return.
      */
     public function invokeMethod(&$object, $methodName, array $parameters = array())
