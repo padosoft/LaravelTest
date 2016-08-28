48c7b00be0820c1451e3f4f9252c076eef1b34ca
lopadova <lorenzo.padovani@padosoft.com>
2016-08-28 18:36:13 +0200

    # 2.0.0 - 2016-08-28 - ADD support for php>=7.0.0 - REMOVE support for php<7.0.0 - CHANGE TestBase now use ReflectionTestable, ExceptionTestable, FileSystemTestable Padosoft\Test\traits traits - CHANGE update composer.json

--- src/TestBase.php
+++ src/TestBase.php
@@ -8,8 +8,14 @@
 
 namespace Padosoft\LaravelTest;
 
+use Padosoft\Test\traits\ExceptionTestable;
+use Padosoft\Test\traits\FileSystemTestable;
+use Padosoft\Test\traits\ReflectionTestable;
+
 class TestBase extends \TestCase
 {
+    use ReflectionTestable, ExceptionTestable, FileSystemTestable;
+
     /**
      * The base URL to use while testing the application.
      *
@@ -23,25 +29,5 @@ class TestBase extends \TestCase
     public function setUp()
     {
         parent::setUp();
-    }
-
-
-    /**
-     * Call protected/private method of a class.
-     *
-     * @param object &$object    Instantiated object that we will run method on.
-     * @param string $methodName Method name to call
-     * @param array  $parameters Array of parameters to pass into method.
-     *
-     *
-     * @return mixed Method return.
-     */
-    public function invokeMethod(&$object, $methodName, array $parameters = array())
-    {
-        $reflection = new \ReflectionClass(get_class($object));
-        $method = $reflection->getMethod($methodName);
-        $method->setAccessible(true);
-
-        return $method->invokeArgs($object, $parameters);
     }
 }
